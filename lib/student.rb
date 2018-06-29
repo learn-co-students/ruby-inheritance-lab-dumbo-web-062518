3c75302763bc9e64fa6c08456f5c2c0cd6b5d087
Dan Chung <danetix@gmail.com>
2018-06-28 23:38:19 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require 'user.rb'
 
+class Student < User
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
